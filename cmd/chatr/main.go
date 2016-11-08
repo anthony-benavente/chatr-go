f899f28af3579b2621f708c3f26b30732c9b54e6
Anthony Benavente <anthony.m.benavente@gmail.com>
2016-11-08 16:19:45 -0500

    Added cl args to specify username and host:port

--- cmd/chatr/main.go
+++ cmd/chatr/main.go
@@ -3,17 +3,40 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
+
+	"strings"
+
+	"strconv"
 
 	chatr "github.com/anthony-benavente/chatr-go"
 )
 
+func usageQuit() {
+	log.Fatal("Usage: chatr <username> <host>:<port>")
+}
+
 func main() {
+	if len(os.Args) < 3 {
+		usageQuit()
+	}
+
+	var host string
+	var port int
+
+	if addr := strings.Split(os.Args[2], ":"); len(addr) == 2 {
+		host = addr[0]
+		if pport, err := strconv.Atoi(addr[1]); err != nil {
+			usageQuit()
+		} else {
+			port = pport
+		}
+	}
+
 	stdin := bufio.NewReader(os.Stdin)
-
-	client := chatr.NewClient("localhost", 8080)
-	client.Start()
-
+	client := chatr.NewClient(host, port)
+	client.Start(os.Args[1])
 	go func() {
 		for data := range client.Incoming {
 			fmt.Println(data)
